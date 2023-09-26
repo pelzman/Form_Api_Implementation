da3b6e61fba38fadea3517fec564b9dd4639bf14
peak2 <36886305+peak2@users.noreply.github.com>
2023-09-26 14:42:32 +0100

    react, tailwind,typescript & antD setup

---
+++ form_api_task1/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "antd"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <Button className="">
+        save me
+      </Button>
+    </div>
+  )
+}
+
+export default App
